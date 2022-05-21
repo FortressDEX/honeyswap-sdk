add78cd9836766a8f138ff3140771934f354e1dd
samisbakedham <samsafahi@gmail.com>
2022-05-21 16:02:23 -0700

    Update candleConstants.ts

--- src/candleConstants.ts
+++ src/candleConstants.ts
@@ -1,6 +1,6 @@
 export const coreCandle = {
   deployer: '',
-  factory: '',
+  factory: '0x251cd2C527Eca282a8C6A4Ef75EfAb19A9Ac5796',
   feeReceiver: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c',
   feeSetter: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c'
 }
