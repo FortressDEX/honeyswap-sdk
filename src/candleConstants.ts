a3a63a4f2c54fe64609e9c34c999139b095ac033
samisbakedham <samsafahi@gmail.com>
2022-05-21 16:29:47 -0700

    Update candleConstants.ts

--- src/candleConstants.ts
+++ src/candleConstants.ts
@@ -1,10 +1,10 @@
 export const coreCandle = {
-  deployer: '',
+  deployer: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c',
   factory: '0x251cd2C527Eca282a8C6A4Ef75EfAb19A9Ac5796',
   feeReceiver: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c',
   feeSetter: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c'
 }
 
 export const peripheryCandle = {
-  router: '0xbc138bd20c98186cc0342c8e380953af0cb48ba8'
+  router: '0x252D8FDAA323094fe43cCa93ad8e650fC72521A7'
 }
