b2f086986faf5c3e5914014ec92e53754e5cc905
Michael <myong@seznam.cz>
2022-05-22 20:55:55 +0200

    Fix of Honey receiver on candle

--- src/candleConstants.ts
+++ src/candleConstants.ts
@@ -1,7 +1,7 @@
 export const coreCandle = {
   deployer: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c',
   factory: '0x251cd2C527Eca282a8C6A4Ef75EfAb19A9Ac5796',
-  feeReceiver: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c',
+  feeReceiver: '0xFf822C4225b76585ab5eE79faF8165742157b31D',
   feeSetter: '0x546D090bbcEC3d96903d41e38C3436c1C601AF9c'
 }
 
